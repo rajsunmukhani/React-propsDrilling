14a7cee2ed3d6086ad8fd7d2c27cf8739dbeec51
Raj <rajsunmukhani123@gmail.com>
2024-05-21 20:21:05 +0530

    made data visible and updation undergo

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,12 @@ const App = () => {
         localStorage.setItem("toDoTasks",JSON.stringify([...tasks,newToDo]))
     }
 
-    console.log(tasks);
+    const completeHandler = (index) => {
+        const copyTasks = [...tasks];
+        copyTasks[index].completed = !copyTasks[index].completed;
+        setTasks(copyTasks);
+        localStorage.setItem("toDoTasks",JSON.stringify(copyTasks));
+    }
 
     return (
         <div className="overflow-x-hidden border-t-2 w-screen min-h-[100vh] bg-zinc-800 flex  items-center flex-col">
@@ -25,7 +30,7 @@ const App = () => {
                     <p>Keeps doing things</p>
                 </div>
                 <div className="text-3xl font-extrabold flex justify-center items-center w-[10vmax] h-[10vmax] rounded-full bg-orange-600">
-                    0/{tasks.length}
+                    {tasks.filter((t) => t.completed === true).length}/{tasks.length}
                 </div>
             </div>
             {/*  */}
@@ -44,38 +49,27 @@ const App = () => {
             {/*  */}
 
             <ul className="list-none w-[35%] ">
-                <li className="mb-5 flex justify-between items-center border rounded-xl p-5">
-                    <div className="flex items-center">
-                        <div
-                            className={`border mr-4 rounded-full w-[30px] h-[30px]  border-orange-600`}
-                        ></div>
-                        <h1
-                            className={` text-2xl font-extrabold text-yellow-100`}
-                        >
-                            Task 01
-                        </h1>
-                    </div>
-                    <div className="flex gap-3 text-2xl text-yellow-100">
-                        <i className="ri-file-edit-line"></i>
-                        <i className="ri-delete-bin-3-line"></i>
-                    </div>
-                </li>
-                <li className="mb-5 flex justify-between items-center border rounded-xl p-5">
-                    <div className="flex items-center">
-                        <div
-                            className={`bg-green-600 mr-4 rounded-full w-[30px] h-[30px]  border-orange-600`}
-                        ></div>
-                        <h1
-                            className={`line-through text-2xl font-extrabold text-yellow-100`}
-                        >
-                            Task 01
-                        </h1>
-                    </div>
-                    <div className="flex gap-3 text-2xl text-yellow-100">
-                        <i className="ri-file-edit-line"></i>
-                        <i className="ri-delete-bin-3-line"></i>
-                    </div>
-                </li>
+                {tasks.length > 0 ? tasks.map((task,index) =>{
+                     return(
+                         <li key={task.id} className="mb-5 flex justify-between items-center border rounded-xl p-5">
+                     <div className="flex items-center">
+                         <div
+                         onClick={() => completeHandler(index)}
+                             className={ task.completed === false ? `border mr-4 rounded-full w-[30px] h-[30px]  border-orange-600` : `bg-green-600 mr-4 rounded-full w-[30px] h-[30px]  border-orange-600`}
+                         ></div>
+                         <h1
+                             className={ task.completed === false ? ` text-2xl font-extrabold text-yellow-100` : `line-through text-2xl font-extrabold text-yellow-100`}
+                         >
+                             {task.title}
+                         </h1>
+                     </div>
+                     <div className="flex gap-3 text-2xl text-yellow-100">
+                         <i className="ri-file-edit-line"></i>
+                         <i className="ri-delete-bin-3-line"></i>
+                     </div>
+                 </li>)
+                 
+                }): <h1 className="mt-10 text-orange-600 text-center w-full text-3xl">No Tasks Pending</h1>}
             </ul>
         </div>
     );
